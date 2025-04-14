2472e11077688aae03333dd01eeb10e7bfd81193
Wasif Israr <50007050+WasifIsrar@users.noreply.github.com>
2025-04-14 16:26:05 +0500

    Initial Commit'

---
+++ cypress/e2e/MyFirstTest.cy.js
@@ -0,0 +1,12 @@
+describe('My First Test',() =>{
+    it('LogsIn',()=> {
+        cy.visit('https://uatv2-dashboard.dev.aioapp.com/')
+        cy.get('input[name="email"]').clear()
+        cy.get('input[name="email"]').type('autobotuatA@gmail.com')
+        cy.get('input[name="password"]').clear()
+        cy.get('input[name="password"]').type('Test@1234')
+        cy.get('.mui-z9exsg').click()
+
+
+    } )
+})
